9f12b540f10d7b18fcc6576a5ebc4038c4ba48cf
michaelpetersen22 <michaelpetersencode@outlook.com>
2022-06-16 13:52:39 +1000

    Implemented Function Library for Weather

--- src/components/Weather.jsx
+++ src/components/Weather.jsx
@@ -1,47 +1,42 @@
-import React from "react";
-import d from "../assets/default.json";
+import { useEffect, useState } from "react";
+import def from "../assets/default.json";
 import { callIntervalImmediately, cache } from "../scripts/Helpers";
+import { fetchWeatherData, weatherDisplay } from "../scripts/OpenWeatherAPI";
 
 export default function Weather() {
-  const [data, setData] = React.useState(d.WeatherDefault);
-  const [output, setOutput] = React.useState("simple");
-
+  const [weather, setWeather] = useState(def.WeatherDefault);
+  const [output, setOutput] = useState("simple");
 
   /**
    * Fetch Weather Data API Caller
    */
-  React.useEffect(() => {
+  useEffect(() => {
     const updateInterval = callIntervalImmediately(() => {
-      //function goes here
+      fetchWeatherData().then((data) => {
+        setWeather(data);
+        });
     }, 30 * 1000);
 
+
+    
     return () => {
       clearInterval(updateInterval);
     };
   }, []);
 
-  /**
-   * API Response Logger
-   */
-  React.useEffect(() => {
-    if (data.cod === 429) {
-      console.log("Too many requests");
-    } else {
-      console.log(data);
-    }
-  }, [data]);
-
   return (
-    <div id="weather" onClick=
-      {() => {
+    <div
+      id="weather"
+      onClick={() => {
         if (output === "simple") {
           setOutput("detailed");
         } else {
           setOutput("simple");
         }
         return output;
-      }}>
-
+      }}
+    >
+      {weatherDisplay(weather, output)}
     </div>
   );
 }
