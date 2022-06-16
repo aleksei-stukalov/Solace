21ed0ed088dda9396815234638788246e927d5d9
michaelpetersen22 <michaelpetersencode@outlook.com>
2022-06-16 22:26:55 +1000

    Update Home.jsx

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,4 +1,4 @@
-import React from 'react'
+import React from "react";
 
 import Clock from "../components/Clock";
 import Greeting from "../components/Greeting";
@@ -12,10 +12,10 @@ export default function Home() {
   return (
     <div>
       <Background />
-      <span id="homeTxt">
-        <Greeting name="Michael" />
-        <Clock timeFormat="24" dateFormat="textual" />
-        <Weather />
+        <span id="homeTxt">
+          <Greeting name="Michael" />
+          <Clock timeFormat="24" dateFormat="textual" />
+          <Weather />
         </span>
     </div>
   );
