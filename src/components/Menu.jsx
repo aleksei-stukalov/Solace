15fffddae7065b9c3840181183dad995119b7878
Alek Groove <stukalov.nz@gmail.com>
2022-07-18 11:29:32 +1200

    Create Menu.jsx
    Navigation bar for user settings. Needs to be separated in its own component.

---
+++ src/components/Menu.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+export default function Menu() {
+  return (
+    <nav>
+      <button>
+        <img src="./icons/svg/cog.svg" alt="" />
+      </button>
+    </nav>
+  );
+}
