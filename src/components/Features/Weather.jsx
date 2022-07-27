16dc9a219f49e92a34ef7a18c6b1315f7e764a4f
Alek Groove <stukalov.nz@gmail.com>
2022-07-28 06:27:40 +1200

    Components migration
    moved some components to new folders,

--- src/components/Features/Weather.jsx
+++ src/components/Features/Weather.jsx
@@ -1,11 +1,11 @@
 import { useEffect, useState } from "react";
 
-import { callIntervalImmediately } from "../scripts/Helpers";
-import { fetchWeatherData, WeatherDisplay } from "../scripts/OpenWeatherAPI";
+import { callIntervalImmediately } from "../../scripts/Helpers";
+import { fetchWeatherData, WeatherDisplay } from "../../scripts/OpenWeatherAPI";
 
 // Importing default Weather Data as workaround to current issue of Weather API
 // Not Immediately Updating
-import def from "../assets/default.json";
+import def from "../../assets/default.json";
 
 /**
  * @description Weather Component that displays weather data in simple and detailed
@@ -18,7 +18,6 @@ export default function Weather() {
   /**
    * Fetch Weather Data API Caller
    */
-
 
   //TODO Attach Interval to Global Timer
   //TODO Verify Data Parsing is correct
