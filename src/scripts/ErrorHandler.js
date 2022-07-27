89d812187acdaedd7d0c78f038f54e203345099e
Alek Groove <stukalov.nz@gmail.com>
2022-07-28 06:24:55 +1200

    Create ErrorHandler.js
    Script to handle errors. Needs to be worked on.

---
+++ src/scripts/ErrorHandler.js
@@ -0,0 +1,9 @@
+export default function ErrorHandler() {
+    // TODO
+    // [ ] Way to reference where the error came from.
+    // [ ] Some code system to identify errors.
+    // [ ] Console description of the error.
+
+    console.log('Error accured');
+    return <p class='error'>Something went wrong!</p>;
+}
