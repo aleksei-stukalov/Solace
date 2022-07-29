b4f64032b9c3f708ab9fa9c43c80efe6e6e92e9d
Alek Groove <stukalov.nz@gmail.com>
2022-07-29 23:25:56 +1200

    Temp menu layout made.
    Only for starting.

--- src/components/Menu.jsx
+++ src/components/Menu.jsx
@@ -1,11 +1,20 @@
 import React from "react";
+import { useState } from "react";
 
 export default function Menu() {
+  const [hide, toggleHide] = useState(false);
+  let menuAppearance = hide == true ? "hidden" : "displayed";
+
   return (
-    <nav>
-      <button>
-        <img src="./icons/svg/cog.svg" alt="" />
-      </button>
+    <nav className={menuAppearance}>
+      <button onClick={() => toggleHide(!hide)}></button>
+      <ul id="menu" className={menuAppearance}>
+        <li>Group A</li>
+        <li>Group B</li>
+        <li>Group C</li>
+        <li>Group D</li>
+        <li>Group E</li>
+      </ul>
     </nav>
   );
 }
