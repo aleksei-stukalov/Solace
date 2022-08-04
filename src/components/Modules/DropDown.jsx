87c50747590fb4e7e7b6c6aeb89c277b9833fb78
Alek Groove <stukalov.nz@gmail.com>
2022-08-04 13:29:17 +1200

    Update DropDown.jsx
    Somewhat working

--- src/components/Modules/DropDown.jsx
+++ src/components/Modules/DropDown.jsx
@@ -1,10 +1,37 @@
 import React from "react";
 
-export default function DropDown({ listItems }) {
-  console.log(React.Children.forEach((i) => console.log(i)));
+export default function DropDown({ passedObject }) {
+  //  passedObject is an object with following attributes:
+  //    list = []                List of elements in the dropdown menu
+  //    selected = <number>      Index of the item in the list as a selected
+  //                             element
+
+  //  TODO
+  // [ ] Figure out input data to this module
+  // [ ] Implement children input
+  // [ ] Add data property to style css variable
+
+  //  Input Data should have:
+  // Some styling (global and local). Global would be theme and such,
+  // when local is going to be some stuff passed to the component, like:
+  // width, type of child hover interaction.
+
+  //  Input elements
+  const elementList = passedObject.list;
+  let elementSelected = passedObject.selected;
+
+  //  Input styling
+  const moduleSize = passedObject.size || "12rem";
+
+  //  List items logic
+  const drawElement = (element) => {
+    return <li>{element}</li>;
+  };
+
   return (
-    <ul className="module-dropdown">
-      <li>asd</li>
-    </ul>
+    <div className="module-dropdown" style={{}}>
+      <button>{elementList[elementSelected]}</button>
+      <ul>{elementList.map((item) => drawElement(item))}</ul>
+    </div>
   );
 }
